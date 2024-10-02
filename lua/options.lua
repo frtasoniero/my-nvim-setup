799939faa6b4719a3d670a1170195cdd4f63bc00
Felipe R. Tasoniero <frtasoniero@gmail.com>
2024-10-01 23:18:31 -0300

    update ui options

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.expandtab = true
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.relativenumber = true
