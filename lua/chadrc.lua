ee091dcece42c2cee0c169433d78a4a2675b5f62
Felipe R. Tasoniero <frtasoniero@gmail.com>
2024-10-01 22:31:36 -0300

    add dashboard on startup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,13 +6,17 @@
 local M = {}
 
 M.base46 = {
-	theme = "tokyonight",
+  theme = "tokyonight",
   transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = { 
+  load_on_startup = true,
 }
 
 return M
