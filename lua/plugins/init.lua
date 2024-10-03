1134983706f8ee54676e31d34207f6ef811f2111
Felipe R. Tasoniero <frtasoniero@gmail.com>
2024-10-03 20:07:01 -0300

    add noice plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,4 +20,39 @@ return {
             require("configs.treesitter")
         end,
     },
+
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            require("configs.completions")
+        end,
+    },
+
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("configs.noice")
+        end,
+    },
 }
